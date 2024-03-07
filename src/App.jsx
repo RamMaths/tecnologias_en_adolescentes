8556242373c01956cf3048a4b172a9dc6cb25e17
Ramses <ram.hdzven@gmail.com>
2024-03-06 19:52:02 -0600

    main ram

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 
+import Introduccion from './componentes/Introduccion';
+
 function App() {
   return (
     <>
--- src/componentes/Introduccion.jsx
+++ src/componentes/Introduccion.jsx
@@ -1,8 +1,21 @@
+import { 
+    Row,
+    Col
+} from 'react-bootstrap';
+
 const Introduccion = () => {
     return (
-    <>
+    <div>
+        <Row className="row row-cols-2">
+            <Col>
+                Hello
+            </Col>
 
-    </>
+            <Col>
+                Hello
+            </Col>
+        </Row>
+    </div>
     );
 }
 
