7050d75d606031ac09db365cdecf21460301a77a
GonzaloMM <gonzalomm1@outlook.com>
2024-03-07 10:47:43 -0600

    Creación del componente Consejos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import '../src/componentes/App.css';
+import React from 'react';
+import Recursos from './componentes/Recursos';
+import Consejos from './componentes/Consejos'; // Import Consejos component
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div>
+      <Recursos />
+      <Consejos />
+    </div>
+  );
 }
 
-export default App
+export default App;
